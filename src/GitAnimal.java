51a5baf0b3d6a2a8a433542575b26bbf6dbffa84
Jmolin910 <jamolin@quinnipiac.edu>
2023-02-27 23:06:11 -0500

    third step

--- src/GitAnimal.java
+++ src/GitAnimal.java
@@ -8,22 +8,23 @@ public class GitAnimal {
 
         String selectedAnimal = "";
 
-         while ( !selectedAnimal.equals("dog") && !selectedAnimal.equals("cat"))   
+         while ( !selectedAnimal.equals("dog") && !selectedAnimal.equals("cat")){   
            
             System.out.println("Would you like to see a dog or a cat?");
             selectedAnimal = inputScanner.next();
          
             if (selectedAnimal.equals("dog"))
-                {
-                    System.out.println("");
-                }
-                else if (selectedAnimal.equals("cat"))
-                {
-                    System.out.println("");
-                }
-                else
-                {
-                    System.out.println("Not available");
-                }
+            {
+                System.out.println("");
+            }
+            else if (selectedAnimal.equals("cat"))
+            {
+                System.out.println("");
+            }
+            else
+            {
+                System.out.println("Not available");
+            }
+        }
     }
 }
